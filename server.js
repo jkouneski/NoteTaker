7b1330e0cf17646eb4610de9308c781568de2641
jkouneski <jkouneski@yahoo.com>
2021-07-25 21:24:59 -0400

    html routes and server file

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 
 const app = express();
-const PORT = process.env || 3001;
+const PORT = process.env.PORT || 3000;
 
 //Set up body parsing, static, and route middleware.
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
+
+//require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+app.listen(PORT, () => console.log(`Listening on PORT: http://localhost:${PORT}`));
 
