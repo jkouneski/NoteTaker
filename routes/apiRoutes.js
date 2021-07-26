af2b428f129106e51a1b26ade52d3762a577b88e
jkouneski <jkouneski@yahoo.com>
2021-07-25 21:55:17 -0400

    api post route & assigning id to notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,4 +5,13 @@ const notes = require("../db/db.json");
 module.exports = (app) => {
     //Filling in API routes
     app.get("/api/notes", (req, res) => res.json(notes));
+
+    app.post("/api/notes", (req, res) => {
+        const addNote = req.body;
+        const noteID = (notes.length).toString();
+        addNote.id = noteID;
+        notes.push(addNote);
+        fs.writeFileSync("./db/db.json", JSON.stringify(notes));
+        return res.json(notes);
+    });
 }
